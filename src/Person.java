4b002730c038d9d29fa3646d3e39968556cbf471
ksamaani <ksamaani@gmail.com>
2026-02-28 01:57:45 +0300

    Person class Finhed

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,33 @@
 public abstract class Person {
+    private int id;
+    private String name;
+    private int age;
+
+    public Person(String name,int id,int age){
+        this.setName(name);
+        this.id = id;
+        this.setAge(age);
+    }
+
+    public abstract double calculateFee();
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
