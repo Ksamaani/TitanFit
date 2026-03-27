93acfb12588246964614fe07cdcd0cba30a833fc
refaei <5rf.moh4@gmail.com>
2026-03-28 02:54:26 +0300

    add a set method and a get method from the workout Plan class  (Composition) and add two variables to The constructor

--- src/PremiumMember.java
+++ src/PremiumMember.java
@@ -1,9 +1,12 @@
 public class PremiumMember extends Member{
     private boolean personalTrainer;
     private boolean spaAccess;
+    private WorkoutPlan customPlan;
 
-    public PremiumMember (int id,String name,int age,int duration){
+    public PremiumMember (int id,String name,int age,int duration, boolean personalTrainer, boolean spaAccess){
         super(id, name, age, duration);
+        this.personalTrainer = personalTrainer;
+        this.spaAccess = spaAccess;
     }
 
     @Override
@@ -17,6 +20,17 @@ public class PremiumMember extends Member{
         }
         return fee;
     }
+
+public void setWorkoutPlan(WorkoutPlan plan) {
+    this.customPlan = plan;
+}
+
+public WorkoutPlan getWorkoutPlan() {
+    return customPlan;
+}
+
+
+
     @Override
     public String toString(){
         return super.toString()+"This is a Premium Member, Has A Personal Trainer: "+ this.personalTrainer+", Has A Spa Access: "+this.spaAccess;
